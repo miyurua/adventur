b18dc9864074d6f0288d89692b9050d1edb97de9
miyuru <miyuruabeysiriwardana13@gmail.com>
2024-07-07 22:35:42 +0530

    shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-5xl underline">Home</h1>
+      <h1 className="text-5xl underline text-white">Home</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
